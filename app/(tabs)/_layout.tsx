353e6a44a5b29a5a30f3f866fa84d38e2472aa46
Andy-J-B <080.abae@gmail.com>
2024-12-19 17:43:25 -0800

    Make base of About us page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -49,6 +49,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="shop"
+        options={{
+          title: "Buy",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="shoppingcart.fill" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
