80580fbaacb128fe0f3d5fe596893b9fc10d6314
Andy-J-B <080.abae@gmail.com>
2024-12-21 11:15:45 -0800

    Install nodemailer

--- backend/controllers/purchaseController.js
+++ backend/controllers/purchaseController.js
@@ -1,1 +1,33 @@
 // purchase main logic
+
+const nodemailer = require("nodemailer");
+
+const makePurchase = async (req, res) => {
+  console.log(req.body);
+};
+
+const sendEmail = async (req, res) => {
+  // Inside your controller after storing purchase
+  //   const transporter = nodemailer.createTransport({
+  //     service: "Gmail",
+  //     auth: {
+  //       user: "your-email@gmail.com",
+  //       pass: "your-email-password",
+  //     },
+  //   });
+  //   const mailOptions = {
+  //     from: "your-email@gmail.com",
+  //     to: "business-owner@example.com",
+  //     subject: "New Purchase Made",
+  //     text: `A purchase was made by ${name}. Quantity: ${quantity}.`,
+  //   };
+  //   transporter.sendMail(mailOptions, (error, info) => {
+  //     if (error) {
+  //       console.error("Error sending email:", error);
+  //     } else {
+  //       console.log("Email sent:", info.response);
+  //     }
+  //   });
+};
+
+module.exports = makePurchase;
--- backend/middleware/purchaseMiddleware.js
+++ backend/middleware/purchaseMiddleware.js
@@ -1,1 +1,22 @@
 // input validation
+
+const verifyPurchase = async (req, res, next) => {
+  const data = req.body;
+  if (
+    !data["name"] ||
+    !data["date"] ||
+    !data["address"] ||
+    !data["phone"] ||
+    !data["email"] ||
+    !data["quantity"] ||
+    !data["transactionInfo"] ||
+    !data["transactionInfo"]["paymentId"] ||
+    !data["transactionInfo"]["status"]
+  ) {
+    return res.status(400).send("Missing required fields");
+  }
+
+  next();
+};
+
+module.exports = verifyPurchase;
